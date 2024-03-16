b126c45e112cba4b8eda2e836828e873d4d86a55
sebseb100 <seisaian@uci.edu>
2024-03-15 17:04:04 -0700

    Updates

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -33,13 +33,13 @@ import tifffile
 class EvalConfig:
     processed_dir: str | os.PathLike = root / "data/processed/4x4"
     raw_dir: str | os.PathLike = root / "data/raw/Train"
-    results_dir: str | os.PathLike = root / "data/predictions" / "FCNResnetTransfer"
+    results_dir: str | os.PathLike = root / "data/predictions" / "SegmentationCNN"
     selected_bands: None = None
     tile_size_gt: int = 4
     batch_size: int = 8
     seed: int = 12378921
     num_workers: int = 11
-    model_path: str | os.PathLike = root / "models" / "FCNResnetTransfer" / "last.ckpt"
+    model_path: str | os.PathLike = root / "models" / "SegmentationCNN" / "last.ckpt"
 
 
 def main(options):
@@ -57,7 +57,6 @@ def main(options):
         tile_size_gt=options.tile_size_gt,
         batch_size=options.batch_size,
         seed=options.seed,
-        num_workers=options.num_workers,
     )
     datamodule.prepare_data()
     datamodule.setup(stage="fit")
@@ -75,10 +74,12 @@ def main(options):
         logger=False,  # Disable logging to avoid unnecessary output
     )
 
-    trainer.validate(model, datamodule=datamodule)
+    # trainer.validate(model, datamodule=datamodule)
 
     # Assuming a method to get validation tile IDs from the datamodule
-    val_tile_ids = datamodule.get_val_tile_ids()
+    val_tile_ids = []
+    for path in Path(options.processed_dir / "4" / "Val" / "subtiles").rglob("*.npz"):
+        val_tile_ids.append(path)
 
     for parent_tile_id in val_tile_ids:
         # Use restitch_and_plot for visualization
@@ -121,8 +122,6 @@ def main(options):
 
 
 if __name__ == "__main__":
-    root = pyprojroot.here()
-    sys.path.append(str(root))
 
     config = EvalConfig()
     parser = ArgumentParser()
@@ -143,6 +142,4 @@ if __name__ == "__main__":
     parser.add_argument(
         "--results_dir", type=str, default=config.results_dir, help="Results directory"
     )
-
-    args = parser.parse_args()
-    main(EvalConfig(**vars(args)))
+    main(EvalConfig(**parser.parse_args().__dict__))
